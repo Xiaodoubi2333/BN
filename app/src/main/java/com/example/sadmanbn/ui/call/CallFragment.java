2023bb265c36060b068bcfdbc2527314a0a9a0eb
Xiaodoubi2333 <crazysnail2133@gmail.com>
2024-05-29 20:40:11 +0800

    第二次提交：call 正确编写fragment.java.

--- app/src/main/java/com/example/sadmanbn/ui/call/CallFragment.java
+++ app/src/main/java/com/example/sadmanbn/ui/call/CallFragment.java
@@ -1,5 +1,9 @@
 package com.example.sadmanbn.ui.call;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,5 +35,33 @@ public class CallFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        new AlertDialog.Builder(getContext())
+                .setTitle("确认")
+                .setMessage("你真的要使用系统自带的拨号程序吗？")
+
+                //
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent=new Intent(Intent.ACTION_DIAL);           //intent，是 系统应用
+                        intent.setData(Uri.parse("tel: "));
+                        startActivity(intent);
+
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 自动关闭对话框
+                    }
+                })
+                .create()
+                .show();
+//
     }
 }
